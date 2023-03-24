62c6609d21ba6a3683c23f07266f1264e0884a63
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-03-24 15:40:15 +0530

    product oerview page UI creaed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodpp/screen/home.dart';
+import 'package:foodpp/screen/productOverview.dart';
 
 import 'auth/signin.dart';
 
--- lib/screen/productOverview.dart
+++ lib/screen/productOverview.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum singlechar { fill, outline }
+
 class ProducOverview extends StatefulWidget {
   const ProducOverview({super.key});
 
@@ -8,15 +10,187 @@ class ProducOverview extends StatefulWidget {
 }
 
 class _ProducOverviewState extends State<ProducOverview> {
+  singlechar _char = singlechar.fill;
+  /*  bottom nav ka function  */
+  Widget bottomnav({
+    required Color iconColor,
+    required Color backgroundColor,
+    required Color color,
+    required String title,
+    required IconData icondata,
+  }) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        color: backgroundColor,
+        child: SizedBox(
+          height: 15,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icondata,
+                size: 17,
+                color: iconColor,
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                title,
+                style: TextStyle(color: color),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Row(
+        children: [
+          bottomnav(
+            backgroundColor: Color.fromARGB(255, 54, 56, 54),
+            /*  bottom nav ka background color */
+
+            color: Colors.white70,
+            iconColor: Colors.grey,
+            /*  icon ka color */
+            title: "Add to Wishlist",
+            /*  botom nav  title */
+            icondata: Icons.favorite_outline, /*  bottom nav ka Icon */
+          ),
+          bottomnav(
+            backgroundColor: Colors.green,
+            /*  bottom nav ka background color */
+
+            color: Color.fromARGB(179, 0, 0, 0),
+            iconColor: Color.fromARGB(255, 0, 0, 0),
+            /*  icon ka color */
+            title: "Add to Cart",
+            /*  botom nav  title */
+            icondata: Icons.shop_outlined, /*  bottom nav ka Icon */
+          )
+        ],
+      ),
       appBar: AppBar(
+        backgroundColor: Colors.green,
         iconTheme: IconThemeData(color: Colors.black87),
         title: Text(
           "Product Overview page",
-          style: TextStyle(color: Colors.black87),
+          style: TextStyle(color: Color.fromARGB(221, 0, 0, 0)),
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("carrot"),
+                    subtitle: Text("\₹15"),
+                  ),
+                  Container(
+                    height: 180,
+                    padding: EdgeInsets.all(10),
+                    child: Image.network(
+                        "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,metadata=none,w=180/app/images/products/sliding_image/332740a.jpg?ts=1679311401"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    width: double.infinity,
+                    child: Text(
+                      "Available Options",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Colors.green, fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 3,
+                              backgroundColor: Colors.grey.shade700,
+                            ),
+                            Radio(
+                              value: singlechar.fill,
+                              groupValue: _char,
+                              activeColor: Colors.green,
+                              onChanged: (value) {
+                                setState(() {
+                                  _char = value!;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        Text("\₹15"),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                size: 17,
+                                color: Colors.green,
+                              ),
+                              Text(
+                                "ADD",
+                                style: TextStyle(color: Colors.black),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            // flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(5),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Product Description",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Nutrient Value & Benefits Contain Fibre, VItamin A, Vitamin K, Carotenoids, Aminoacid.Vitamin A improves eye health and, boost immunity. Fibres helps keeping up the digestion. Vitamin C helps improving skiin and hair health.\nStorage Tips Dry carrots in sun for a few hours after which you can refrigerate by keeping in plastic bag with holes punched in it or mesh bags.",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
